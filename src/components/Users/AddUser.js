a475725464c467abb4441645e09823b81731bfde
mzoric33 <zoricmarin4@gmail.com>
2022-09-12 19:59:05 +0200

    adding_card_component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from "react";
+import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -6,13 +8,15 @@ const AddUser = (props) => {
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"></input>
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="text"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="text"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
